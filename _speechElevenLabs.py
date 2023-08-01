d1d924cc48393c8afcc4fa6a3976ceb2bc0e5ab0
Zayd Khan <43301355+zkhan04@users.noreply.github.com>
2023-08-01 04:50:59 -0500

    Update _speechElevenLabs.py

--- _speechElevenLabs.py
+++ _speechElevenLabs.py
@@ -1,12 +1,15 @@
 import os
-from elevenlabs import generate, set_api_key
+import wave
+from dotenv import load_dotenv
+from elevenlabs import generate, set_api_key, save
+
+load_dotenv()
+XI_API_KEY = os.getenv("XI_API_KEY")
+#Set API key for ElevenLabs
+set_api_key(XI_API_KEY)
 
 #Define function for generating speech
 def generate_speech(text_input, voice_actor):
-
-    #Set API key for ElevenLabs
-    set_api_key(os.getenv("XI_API_KEY"))
-
     generated_speech = generate(
         text = text_input,
         voice = voice_actor,
@@ -14,4 +17,13 @@ def generate_speech(text_input, voice_actor):
     
     return generated_speech
 
-# generate_speech("Hello, my name is Bella. I am a voice from Eleven Labs. I am a text to speech model. ", "Bella")
+def generate_all_speech(text_inputs, voice_actor):
+    all_speeches = [generate_speech(text_input, voice_actor) for text_input in text_inputs]
+    return all_speeches
+
+def save_audios(audios):
+    folderpath = os.getcwd() + "/audios"
+    for idx, audio in enumerate(audios):
+        filename = f"audio{idx}.mp3"
+        file_path = os.path.join(folderpath, filename)
+        save(audio, file_path)
